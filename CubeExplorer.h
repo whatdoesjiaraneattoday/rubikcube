14f42807c62a055019417dd4c2ebeb3af89262ff
“FictiveDandan” <3424793414@qq.com>
2023-12-24 19:05:40 +0800

    dfs完成

--- CubeExplorer.h
+++ CubeExplorer.h
@@ -49,12 +49,12 @@ ostream& operator<<(ostream&, const Operation&);
 class CubeExplorer {
 private:
 	vector<string> vecStrSerial;		//保存转换后的串口序列字符串
-	vector<string> strNorVec;			//string容器，保存分割后得到的一个个表示单个公式操作的字符串
-	vector<Operation> macVec;			//自定义枚举类型Operation容器，保存转换后得到的机械手操作序列
 	string target;						//字符串，存储将要处理的普通公式序列
 	int shortestTime = 99999;
 public:
 	HandState handState;	//自定义类HandState类型，表示当前机械手状态
+	vector<string> strNorVec;			//string容器，保存分割后得到的一个个表示单个公式操作的字符串
+	vector<Operation> macVec;			//自定义枚举类型Operation容器，保存转换后得到的机械手操作序列
 	int transCnt = 0;					//记录魔方翻转次数
 	
 	CubeExplorer(char* cstr="", const HandState& hs=HandState(true,true,true,true));
@@ -65,28 +65,28 @@ public:
 	vector<string>& GetVecStrSerial();
 
 	//以下操作定义针对魔方本身，封装机械爪操作
-	void OnR(vector<string>::iterator&);		//R面顺时针转动
-	void On_R(vector<string>::iterator&);		//R面逆时针转动
-	void OnR2(vector<string>::iterator&);		//R面转动180°
-	void OnF(vector<string>::iterator&);		//F面类似
-	void On_F(vector<string>::iterator&);		//
-	void OnF2(vector<string>::iterator&);		//
-	void OnRR(vector<string>::iterator&);		//魔方沿R面顺时针方向整体转动
-	void On_RR(vector<string>::iterator&);		//魔方沿R面逆时针方向整体转动
-	void OnRR2(vector<string>::iterator&);		//魔方沿R面逆时针方向整体转动180°
-	void OnFF(vector<string>::iterator&);		//F类似
-	void On_FF(vector<string>::iterator&);		//
-	void OnFF2(vector<string>::iterator&);		//
+	void OnR(vector<string>& operation_seq, vector<Operation>& maneuver);		//R面顺时针转动
+	void On_R(vector<string>& operation_seq, vector<Operation>& maneuver);		//R面逆时针转动
+	void OnR2(vector<string>& operation_seq, vector<Operation>& maneuver);		//R面转动180°
+	void OnF(vector<string>& operation_seq, vector<Operation>& maneuver);		//F面类似
+	void On_F(vector<string>& operation_seq, vector<Operation>& maneuver);		//
+	void OnF2(vector<string>& operation_seq, vector<Operation>& maneuver);		//
+	void OnRR(vector<string>& operation_seq, vector<Operation>& maneuver);		//魔方沿R面顺时针方向整体转动
+	void On_RR(vector<string>& operation_seq, vector<Operation>& maneuver);		//魔方沿R面逆时针方向整体转动
+	void OnRR2(vector<string>& operation_seq, vector<Operation>& maneuver);		//魔方沿R面逆时针方向整体转动180°
+	void OnFF(vector<string>& operation_seq, vector<Operation>& maneuver);		//F类似
+	void On_FF(vector<string>& operation_seq, vector<Operation>& maneuver);		//
+	void OnFF2(vector<string>& operation_seq, vector<Operation>& maneuver);		//
 
-	void GetLeftReadyAndTight();	//使左手复位至夹紧且U-D状态(90/270°状态)
-	void GetRightReadyAndTight();	//使右手复位至夹紧且U-D状态(90/270°状态)
-	void LeftLoose();				//松开左手
-	void LeftTight();				//夹紧左手
-	void LeftReady();				//左手复位至U-D状态(90/270°状态)
-	void RightLoose();				//松开右手
-	void RightTight();				//夹紧右手
-	void RightReady();				//右手复位至U-D状态(90/270°状态)
-	void GetShortestWay();			//核心成员函数，将target中存储的普通公式序列转换为机械手操作序列并存储到macVec容器中
+	void GetLeftReadyAndTight(vector<Operation>& maneuver);	//使左手复位至夹紧且U-D状态(90/270°状态)
+	void GetRightReadyAndTight(vector<Operation>& maneuver);	//使右手复位至夹紧且U-D状态(90/270°状态)
+	void LeftLoose(vector<Operation>& maneuver);				//松开左手
+	void LeftTight(vector<Operation>& maneuver);				//夹紧左手
+	void LeftReady(vector<Operation>& maneuver);				//左手复位至U-D状态(90/270°状态)
+	void RightLoose(vector<Operation>& maneuver);				//松开右手
+	void RightTight(vector<Operation>& maneuver);				//夹紧右手
+	void RightReady(vector<Operation>& maneuver);				//右手复位至U-D状态(90/270°状态)
+	//void GetShortestWay();			//核心成员函数，将target中存储的普通公式序列转换为机械手操作序列并存储到macVec容器中
 	void ShowOperations();			//打印操作序列，主要用于调试和测试
 	int GetOperationTime(vector<Operation> maneuver);         //获取完成操作序列需要的时间
 	void GetStrNorVec();            //将魔方序列字符串分割为单个字符
